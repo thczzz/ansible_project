0646322878930411384eb296fe840ca5e60f91d1
echonykrom <echonykrom@UbuntuVM.myguest.virtualbox.org>
2023-07-05 13:58:45 +0300

    test

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 ssh -o StrictHostKeyChecking=no -i ../../webserver.pem ubuntu@52.90.170.30 'cd ansible_project &&
        ansible-playbook vpro_ec2_stack.yaml &&
        ansible-playbook -i inventory-bastion bastion_setup.yaml &&
-       ./script_part2.sh && exit 0 && exit; exec bash -lxe'
+       ./script_part2.sh && exit 0 && exit; exec bash -lx'
