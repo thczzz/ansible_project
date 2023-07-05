87ffa22075229fe17fe9d53ea17fa6ef2c149983
echonykrom <echonykrom@UbuntuVM.myguest.virtualbox.org>
2023-07-05 12:46:49 +0300

    testing

--- script.sh
+++ script.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-ssh -o StrictHostKeyChecking=no -i ./bastion-key.pem ubuntu@3.90.207.141 'cd ansible_project/provision_stack && echo "executing ansible-playbook" && sudo ansible-playbook -v site.yaml && exit; exec bash -l'
+ssh -o StrictHostKeyChecking=no -i ../../webserver.pem ubuntu@52.90.170.30 'cd ansible_project &&
+       ansible-playbook site.yaml && 
+       ansible-playbook vpro_ec2_stack.yaml &&
+       ansible-playbook -i inventory-bastion bastion_setup.yaml &&
+       ./script_part2.sh && exit 0 && exit; exec bash -lxe'
