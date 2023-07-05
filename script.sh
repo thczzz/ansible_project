7d5084fec896b97c090d0fa194ddb4a71075cc57
echonykrom <echonykrom@UbuntuVM.myguest.virtualbox.org>
2023-07-05 16:47:26 +0300

    finished

--- script.sh
+++ script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-ssh -o StrictHostKeyChecking=no -i ../../webserver.pem ubuntu@52.90.170.30 'cd ansible_project &&
-       ansible-playbook vpro_ec2_stack.yaml &&
-       ansible-playbook -i inventory-bastion bastion_setup.yaml &&
+function cleanup() {
+  echo "Exiting.."
+}
+ssh -o StrictHostKeyChecking=no -i ../../webserver.pem ubuntu@54.159.128.211 'cd ansible_project &&
+       sudo ansible-playbook site.yaml &&
+       sudo ansible-playbook vpro_ec2_stack.yaml &&
+       sudo ansible-playbook bastion_setup.yaml && 
        ./script_part2.sh && exit 0 && exit; exec bash -lx'
+
+trap 'trap SIGINT; kill -s SIGINT "$$"' SIGINT; trap cleanup EXIT
